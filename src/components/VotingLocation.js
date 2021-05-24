d6bac724789b3b6502a9efc7cc7a034c85b3202a
Jarquevious Nelson <jarquevious@gmail.com>
2021-05-24 00:22:33 -0700

    api renders election data

--- src/components/VotingLocation.js
+++ src/components/VotingLocation.js
@@ -10,21 +10,21 @@ function VotingLocation() {
 
   // ----------------------------------------------------------------
   async function getLocationByZip() {
-    console.log('hey')
+    
     const key = "AIzaSyCPi_BBmvysukkWehCrXv9eBlN8k2miiF4";
-    const path =  `GET https://www.googleapis.com/civicinfo/v2/voterinfo?key=${key}`;
+    // const path =  `https://www.googleapis.com/civicinfo/v2/voterinfo?key=${key}`;
+    // const path = `https://www.googleapis.com/civicinfo/v2/elections?key=${key}`
+    const path = `https://www.googleapis.com/civicinfo/v2/voterinfo?key=${key}&address=${zip}&electionId=2000`
     try {
+      
       console.log(path)
       const res = await fetch(path); // stop !
       const json = await res.json(); // stop !
-      
+      console.log("---json---")
       console.log(json); 
-      const pollingLocation = json.pollingLocations;
-      
-      
-
-      setData({pollingLocation});
+      setData(json);
     } catch (err) {
+      console.log("---error---")
       console.log(err.message);
     }
   }
@@ -41,8 +41,11 @@ function VotingLocation() {
             />
             <button type="submit">Submit</button>
           </form>
-          { data && data.voterinfo.map( (election) => <p>{election.name}</p>) }
+          { data && data.contests.map( (election) => <p>{election.office}</p>) }
       </div>
   )
 }
 export default VotingLocation
+
+
+// curl "https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyCPi_BBmvysukkWehCrXv9eBlN8k2miiF4&address=851%20california%20st.%20San%20Francisco%20CA&electionId=2021"
